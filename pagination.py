cfd22283aa4b4237be95221bc60325afaeb1926f
lewisj <lewisj@users.noreply.github.com>
2025-04-25 10:48:26 +0100

    added regex and made it clearer where API key goes

--- pagination.py
+++ pagination.py
@@ -1,6 +1,7 @@
 import requests
+import re
 
-os_api_key = ''
+os_api_key = 'YOUR API KEY HERE'
 your_bounding_box = '-0.101472,51.522807,-0.096827,51.526016'
 
 os_ngd_url = "https://api.os.uk/features/ngd/ofa/v1/collections/bld-fts-building-1/items"
@@ -9,8 +10,16 @@ parameters = {"key": os_api_key, "bbox": your_bounding_box}
 all_fetched_features = []
 os_response = requests.get(url=os_ngd_url, params=parameters).json()
 
-if len(os_api_key) < 10:
-    print("Key is too short, please check your API key")
+
+def is_valid_os_api_key(os_api_key):
+    if re.match(r'^([a-zA-Z0-9]){32}$', os_api_key):
+        return True
+    else:
+        print("Please enter a valid OS API Key")
+        return False
+
+is_valid_os_api_key(os_api_key)
+
 
 while True:
     all_fetched_features.extend(os_response['features'])
