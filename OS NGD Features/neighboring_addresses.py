4176085e2aa275f45c3cca04acc658a345f981f7
MurrayChapman17 <murraychapman17@gmail.com>
2025-05-01 15:29:15 +0100

    line gaps

--- OS NGD Features/neighboring_addresses.py
+++ OS NGD Features/neighboring_addresses.py
@@ -10,9 +10,10 @@ ADDRESS = 'F4, Sutton Yard, 65 Goswell Rd., London EC1V 7EN'
 
 QUERY_URI = 'https://api.os.uk/search/places/v1/find?query=' + ADDRESS
 query_response = requests.get(QUERY_URI, params={'key': API_KEY}, timeout=10).json()
-first_result = query_response['results'][0]['DPA']
 
+first_result = query_response['results'][0]['DPA']
 x, y = first_result['X_COORDINATE'], first_result['Y_COORDINATE']
+
 # 300m radius search
 RADIUS_URI = f'https://api.os.uk/search/places/v1/radius?point={x},{y}&radius=300'
 
