27ec70b04c1bcbec35a9e38ca162ba141c410619
MurrayChapman17 <murraychapman17@gmail.com>
2025-05-02 12:00:51 +0100

    refactoring params

--- neighboring_addresses.py
+++ neighboring_addresses.py
@@ -7,16 +7,29 @@ import requests
 API_KEY = 'YOUR_API_KEY'
 # Replace with your address
 ADDRESS = 'F4, Sutton Yard, 65 Goswell Rd., London EC1V 7EN'
+RADIUS = 300  # in meters
 
-QUERY_URI = 'https://api.os.uk/search/places/v1/find?query=' + ADDRESS
+query_uri = 'https://api.os.uk/search/places/v1/find'
 
 # Get the coordinates of the address
-query_response = requests.get(QUERY_URI, params={'key': API_KEY}, timeout=10).json()
+query_response = requests.get(
+    query_uri,
+    params={'key': API_KEY, 'query': ADDRESS},
+    timeout=10
+).json()
 first_result = query_response['results'][0]['DPA']
 x, y = first_result['X_COORDINATE'], first_result['Y_COORDINATE']
 
 # 300m radius search
-RADIUS_URI = f'https://api.os.uk/search/places/v1/radius?point={x},{y}&radius=300'
-radius_response = requests.get(RADIUS_URI, params={'key': API_KEY}, timeout=10).json()
+radius_uri = f'https://api.os.uk/search/places/v1/radius'
+radius_response = requests.get(
+    radius_uri,
+    params={
+        'key': API_KEY,
+        'point': f'{x},{y}',
+        'radius': RADIUS,
+    },
+    timeout=10
+).json()
 
 print(radius_response)
