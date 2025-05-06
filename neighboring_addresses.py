9e6c391e8a4b2cfe82246c1275a041cd59d1f0a3
MurrayChapman17 <murraychapman17@gmail.com>
2025-05-06 15:28:31 +0100

    refactoring

--- neighboring_addresses.py
+++ neighboring_addresses.py
@@ -5,31 +5,47 @@ import requests
 
 # Replace with your API key
 API_KEY = 'YOUR_API_KEY'
-# Replace with your address
-ADDRESS = 'F4, Sutton Yard, 65 Goswell Rd., London EC1V 7EN'
-RADIUS = 300  # in meters
 
-query_uri = 'https://api.os.uk/search/places/v1/find'
+def get_nearby_addresses(address, radius):
+    """
+    Get nearby addresses within a specified radius from a given address.
+    :param address: The address to search from.
+    :param radius: The radius in meters to search for nearby addresses.
+    :return: A list of nearby addresses.
+    """
+    query_uri = 'https://api.os.uk/search/places/v1/find'
 
-# Get the coordinates of the address
-query_response = requests.get(
-    query_uri,
-    params={'key': API_KEY, 'query': ADDRESS},
-    timeout=10
-).json()
-first_result = query_response['results'][0]['DPA']
-x, y = first_result['X_COORDINATE'], first_result['Y_COORDINATE']
+    # Get the coordinates of the address
+    query_response = requests.get(
+        query_uri,
+        params={'key': API_KEY, 'query': address},
+        timeout=10
+    ).json()
+    first_result = query_response['results'][0]['DPA']
+    x, y = first_result['X_COORDINATE'], first_result['Y_COORDINATE']
 
-# 300m radius search
-radius_uri = f'https://api.os.uk/search/places/v1/radius'
-radius_response = requests.get(
-    radius_uri,
-    params={
-        'key': API_KEY,
-        'point': f'{x},{y}',
-        'radius': RADIUS,
-    },
-    timeout=10
-).json()
+    # Radius search
+    radius_uri = 'https://api.os.uk/search/places/v1/radius'
+    radius_response = requests.get(
+        radius_uri,
+        params={
+            'key': API_KEY,
+            'point': f'{x},{y}',
+            'radius': radius,
+        },
+        timeout=10
+    ).json()
+    return radius_response
 
-print(radius_response)
+def run():
+    """
+    Main function to run the function with example input.
+    """
+    # Replace with your address
+    address = 'F4, Sutton Yard, 65 Goswell Rd., London EC1V 7EN'
+    radius = 300  # in meters
+    # Get the neighboring addresses
+    radius_response = get_nearby_addresses(address, radius)
+    print(radius_response)
+
+run()
