9f55b5cffc6a66ec420281049d46c25b8b638324
lewisj <lewisj@users.noreply.github.com>
2025-05-08 14:47:44 +0100

    Fixed error with key

--- Joining our APIs together/site to Road.py
+++ Joining our APIs together/site to Road.py
@@ -17,44 +17,46 @@ def fetch_data_from_os_api(collection, ids, id_type):
     }
     return requests.get(url=os_ngd_url, params=parameters, timeout=10).json()
 
+def run_match_sites_to_roads():
+    # Here we use a sample OSID to demonstrate the process
+    osids = ['a3677768-e3fc-45ca-b033-66c8e749aa98']
+    # we know this OSID is in the sites collection
+    data = fetch_data_from_os_api("lus-fts-site-2", osids, "osid")
 
-# Here we use a sample OSID to demonstrate the process
-OSIDS = ['a3677768-e3fc-45ca-b033-66c8e749aa98']
-# we know this OSID is in the sites collection
-data = fetch_data_from_os_api("lus-fts-site-2", OSIDS, "osid")
+    uprns_to_join = []
+    if data['features']:
+        for feature in data['features']:
+            if 'sitetoaddressreference' in feature['properties']:
+                uprn_references = feature['properties']['sitetoaddressreference']
+                for uprn_reference in uprn_references:
+                    uprn = uprn_reference['uprn']
+                    uprns_to_join.append(uprn)
+    print("these are the uprns:", uprns_to_join)
 
-UPRNs_to_join = []
-if data['features']:
-    for feature in data['features']:
-        if 'sitetoaddressreference' in feature['properties']:
-            uprn_references = feature['properties']['sitetoaddressreference']
-            for uprn_reference in uprn_references:
-                uprn = uprn_reference['uprn']
-                UPRNs_to_join.append(uprn)
-print("these are the uprns:", UPRNs_to_join)
+    road_link_toids = []
+    linked_id_params = {"key": YOUR_OS_API_KEY}
+    for uprn in uprns_to_join:
+        data = requests.get(f"https://api.os.uk/search/links/v1/featureTypes/BLPU/{uprn}", params=linked_id_params,
+                            timeout=10).json()
+        for correlation in data["correlations"]:
+            if correlation["correlatedFeatureType"] == "RoadLink":
+                for identifier in correlation["correlatedIdentifiers"]:
+                    road_link_toids.append(identifier["identifier"])
 
-road_link_toids = []
-linked_id_params = {"key": YOUR_OS_API_KEY}
-for uprn in UPRNs_to_join:
-    data = requests.get(f"https://api.os.uk/search/links/v1/featureTypes/BLPU/{uprn}", params=linked_id_params,
-                        timeout=10).json()
-    for correlation in data["correlations"]:
-        if correlation["correlatedFeatureType"] == "RoadLink":
-            for identifier in correlation["correlatedIdentifiers"]:
-                road_link_toids.append(identifier["identifier"])
+    # The NGD collection name and version e.g. "trn-ntwk-roadlink-4"
 
-# The NGD collection name and version e.g. "trn-ntwk-roadlink-4"
+    road_link_data = fetch_data_from_os_api("trn-ntwk-roadlink-4", road_link_toids, 'toid')
 
-road_link_data = fetch_data_from_os_api("trn-ntwk-roadlink-4", road_link_toids, 'toid')
+    roadtrackorpath_ids = []
+    for feature in road_link_data["features"]:
+        for reference in feature["properties"].get("roadtrackorpathreference", []):
+            roadtrackorpath_ids.append(reference["roadtrackorpathid"])
+    print("these are the IDs for the road track and path features :", roadtrackorpath_ids)
 
-roadtrackorpath_ids = []
-for feature in road_link_data["features"]:
-    for reference in feature["properties"].get("roadtrackorpathreference", []):
-        roadtrackorpath_ids.append(reference["roadtrackorpathid"])
-print("these are the IDs for the road track and path features :", roadtrackorpath_ids)
+    # The NGD collection name and version e.g. "trn-fts-roadtrackorpath-3"
+    ngd_collection = "trn-fts-roadtrackorpath-3"
+    road_track_and_path_data = fetch_data_from_os_api(ngd_collection, roadtrackorpath_ids, 'osid')
 
-# The NGD collection name and version e.g. "trn-fts-roadtrackorpath-3"
-NGD_COLLECTION = "trn-fts-roadtrackorpath-3"
-road_track_and_path_data = fetch_data_from_os_api(NGD_COLLECTION, roadtrackorpath_ids, 'osid')
+    print(road_track_and_path_data)
 
-print(road_track_and_path_data)
+run_match_sites_to_roads()
