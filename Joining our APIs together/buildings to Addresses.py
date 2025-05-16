1eff1025e354c2437dc285161ad9ee7bf2fdb2f4
lewisj <lewisj@users.noreply.github.com>
2025-05-16 16:33:49 +0100

    Added base function

--- Joining our APIs together/buildings to Addresses.py
+++ Joining our APIs together/buildings to Addresses.py
@@ -10,36 +10,39 @@ bbox = "-0.10004, 51.524194,-0.099471, 51.524494"
 
 NGD_FEATURES_URL = f"https://api.os.uk/features/ngd/ofa/v1/collections/{BUILDINGS_COLLECTION}/items"
 
-uprns_to_fetch = []
 
-parameters = {
-    "key": YOUR_OS_API_KEY,
-    "bbox": bbox
-}
+def match_buildings_to_addressses():
+    uprns_to_fetch = []
 
-os_response = requests.get(url=NGD_FEATURES_URL, params=parameters, timeout=10).json()
+    parameters = {
+        "key": YOUR_OS_API_KEY,
+        "bbox": bbox
+    }
 
-for feature in os_response['features']:
-    uprnreferences = feature['properties'].get('uprnreference')
-    if uprnreferences:
-        for values in uprnreferences:
-            uprns_to_fetch.append(values['uprn'])
+    os_response = requests.get(url=NGD_FEATURES_URL, params=parameters, timeout=10).json()
 
-os_places_url = "https://api.os.uk/search/places/v1/uprn?"
+    for feature in os_response['features']:
+        uprnreferences = feature['properties'].get('uprnreference')
+        if uprnreferences:
+            for values in uprnreferences:
+                uprns_to_fetch.append(values['uprn'])
 
-parameters = {
-    "key": YOUR_OS_API_KEY
-}
+    os_places_url = "https://api.os.uk/search/places/v1/uprn?"
 
-full_addresses = []
+    parameters = {
+        "key": YOUR_OS_API_KEY
+    }
 
-for uprn in uprns_to_fetch:
-    parameters['uprn'] = uprn
-    response = requests.get(url=os_places_url, params=parameters, timeout=10).json()
-    print(requests.get(url=os_places_url, params=parameters, timeout=10).url)
-    if 'results' in response and response['results']:
-        for result in response['results']:
-            full_addresses.append(result['DPA']['ADDRESS'])
+    full_addresses = []
 
-print(full_addresses)
+    for uprn in uprns_to_fetch:
+        parameters['uprn'] = uprn
+        response = requests.get(url=os_places_url, params=parameters, timeout=10).json()
+        print(requests.get(url=os_places_url, params=parameters, timeout=10).url)
+        if 'results' in response and response['results']:
+            for result in response['results']:
+                full_addresses.append(result['DPA']['ADDRESS'])
 
+    print(full_addresses)
+
+match_buildings_to_addressses()
